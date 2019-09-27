9b92ead3b48691de15f39cea27716d3a09eba707
DoughyJoey <32750162+DoughyJoey@users.noreply.github.com>
2019-09-27 17:03:18 +0100

    Set up Firebase Auth & Database and added functionality to Signup & Login activities

---
+++ app/src/main/java/com/example/dogadoptionapp/Util/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.dogadoptionapp.Util
+
+val DATA_USERS = "Users"
---
+++ app/src/main/java/com/example/dogadoptionapp/Util/Data.kt
@@ -0,0 +1,11 @@
+package com.example.dogadoptionapp
+
+data class User(
+    val uid: String? = "",
+    val name: String? = "",
+    val age: String? = "",
+    val email: String? = "",
+    val gender: String? = "",
+    val preference: String? = "",
+    val imageUrl: String? = ""
+)
